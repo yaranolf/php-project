7c75860729e49c8ab57658e61eebfc7d32590bbf
Yara Nolf <yara.nolf@telenet.be>
2019-04-04 23:44:47 +0200

    error try-catch gefixt

--- classes/User.class.php
+++ classes/User.class.php
@@ -81,31 +81,33 @@
             return $this;
         }
 
-        public function register() {
-        $options = [
-            'cost' => 14, //je schrijft 2^14, hash wordt dus 4096x uitgevoerd, hacker moet gokken welke macht wij gekozen hebben, duurt ook langer, met brute force kan je veel minder pogingen doen per minuut
-        ];
-        
-        $password = password_hash($password, PASSWORD_DEFAULT, $options); //PASSWORD_DEFAULT is constant, gaat niet wijzigen
-        
-        try{
-            $conn= new PDO("mysql:host=localhost;dbname=inspiration_hunter;","root","root", null);
-            $statement = $conn->prepare("INSERT INTO users (fullname, email, password) VALUES(:fullname, :email, :password)");
-            //gaat injectie tegen, er is geen $_POST, zijn 2 gaten waar nog iets moet binnenkomen
-            $statement->bindParam(":email", $email);
-            $statement->bindParam(":password", $password);
-            $statement->bindParam(":fullname", $fullName);
-            //plakt niks in query tot je runt, bindValue stopt direct in query (ook zonder runnen)
-            //bindParam gaat quotes negeren
-            $statement->execute();
-            //geeft true of false terug zodat je weet of het gelukt is
-            $result = $statement->execute();
-            echo $result; 
-            //om te zien wat er uit komt
-        } catch (Throwable $t){
-            echo "er liep iets mis";
-            //echo $t->getMessage();
-        }
+        public function register(){
+            $options = [
+                'cost' => 14, //je schrijft 2^14, hash wordt dus 4096x uitgevoerd, hacker moet gokken welke macht wij gekozen hebben, duurt ook langer, met brute force kan je veel minder pogingen doen per minuut
+            ];
+
+            $password = password_hash($this->password, PASSWORD_DEFAULT, $options); //PASSWORD_DEFAULT is constant, gaat niet wijzigen
+            
+            try{
+                $conn= new PDO("mysql:host=localhost;dbname=inspiration_hunter;","root","root", null);
+                $statement = $conn->prepare("INSERT INTO users (fullname, email, password) VALUES(:fullname, :email, :password)");
+                //gaat injectie tegen, er is geen $_POST, zijn 2 gaten waar nog iets moet binnenkomen
+                $statement->bindParam(":email", $email);
+                $statement->bindParam(":password", $password);
+                $statement->bindParam(":fullname", $fullName);
+                //plakt niks in query tot je runt, bindValue stopt direct in query (ook zonder runnen)
+                //bindParam gaat quotes negeren
+                $statement->execute();
+                //geeft true of false terug zodat je weet of het gelukt is
+                $result = $statement->execute();
+                return $result;
+                echo $result; 
+                //om te zien wat er uit komt
+            } catch (Throwable $t){
+                echo "er liep iets mis";
+                return false;
+                //echo $t->getMessage();
+            }
         }
     }
 ?>
