e3a8a4724341bd9c0172ec7d0183fc46c6de5195
Yara Nolf <yara.nolf@telenet.be>
2019-04-04 23:20:31 +0200

    index, nav, register, user class aangemaakt

---
+++ classes/User.class.php
@@ -0,0 +1,110 @@
+<?php
+    class User {
+        private $fullName;
+        private $email;
+        private $password;
+        private $passwordConfirmation;
+
+        /**
+         * Get the value of fullName
+         */ 
+        public function getFullName()
+        {
+            return $this->fullName;
+        }
+
+        /**
+         * Set the value of fullName
+         *
+         * @return  self
+         */ 
+        public function setFullName($fullName)
+        {
+            $this->fullName = $fullName;
+            return $this;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+            $this->email = $email;
+            return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+            return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+            $this->password = $password;
+            return $this;
+        }
+
+        /**
+         * Get the value of passwordConfirmation
+         */ 
+        public function getPasswordConfirmation()
+        {
+            return $this->passwordConfirmation;
+        }
+
+        /**
+         * Set the value of passwordConfirmation
+         *
+         * @return  self
+         */ 
+        public function setPasswordConfirmation($passwordConfirmation)
+        {
+            $this->passwordConfirmation = $passwordConfirmation;
+            return $this;
+        }
+
+        public function register() {
+        $options = [
+            'cost' => 14, //je schrijft 2^14, hash wordt dus 4096x uitgevoerd, hacker moet gokken welke macht wij gekozen hebben, duurt ook langer, met brute force kan je veel minder pogingen doen per minuut
+        ];
+        
+        $password = password_hash($password, PASSWORD_DEFAULT, $options); //PASSWORD_DEFAULT is constant, gaat niet wijzigen
+        
+        try{
+            $conn= new PDO("mysql:host=localhost;dbname=inspiration_hunter;","root","root", null);
+            $statement = $conn->prepare("INSERT INTO users (fullname, email, password) VALUES(:fullname, :email, :password)");
+            //gaat injectie tegen, er is geen $_POST, zijn 2 gaten waar nog iets moet binnenkomen
+            $statement->bindParam(":email", $email);
+            $statement->bindParam(":password", $password);
+            $statement->bindParam(":fullname", $fullName);
+            //plakt niks in query tot je runt, bindValue stopt direct in query (ook zonder runnen)
+            //bindParam gaat quotes negeren
+            $statement->execute();
+            //geeft true of false terug zodat je weet of het gelukt is
+            $result = $statement->execute();
+            echo $result; 
+            //om te zien wat er uit komt
+        }catch (Throwable $t){
+            echo "er liep iets mis";
+            //echo $t->getMessage();
+        }
+    }
+}
---
+++ register.php
@@ -0,0 +1,62 @@
+<?php
+	require_once("classes/User.class.php");
+
+
+	  if(!empty($_POST)){
+            $user = new User();
+            $user->setFullName($_POST['fullname']);
+            $user->setEmail($_POST['email']);
+			$user->setPassword($_POST['password']);
+			$user->setPasswordConfirmation($_POST['password_confirmation']);
+			
+			$user->register();
+			var_dump($result);
+		}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Inspiration Hunter</title>
+  <!--<link rel="stylesheet" href="css/style.css">-->
+</head>
+<body>
+	<div class="inspirationHunterLogin inspirationHunterLogin--register">
+		<div class="form form--login">
+			<form action="" method="post">
+				<h2 form__title>Sign up for an account</h2>
+
+				<div class="form__error hidden">
+					<p>
+						Some error here
+					</p>
+				</div>
+
+				<div class="form__field">
+					<label for="email">Email</label>
+					<input type="text" id="email" name="email">
+                </div>
+                
+                <div class="form__field">
+					<label for="fullname">Full name</label>
+					<input type="text" id="fullname" name="fullname">
+                </div>
+
+				<div class="form__field">
+					<label for="password">Password</label>
+					<input type="password" id="password" name="password">
+				</div>
+
+                <div class="form__field">
+					<label for="password_confirmation">Confirm your password</label>
+					<input type="password" id="password_confirmation" name="password_confirmation">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign me up!" class="btn btn--primary">	
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
