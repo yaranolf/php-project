20c9dc12c4532aa188255e58b58918111aa83294
lissethys <r0448961@student.thomasmore.be>
2019-04-09 14:10:45 +0200

    upload image - versie 2

--- upload.php
+++ upload.php
@@ -1,48 +1,22 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-?>
 
+<?php
+
+if(!empty($_POST)){
+    $description = $_POST['description'];
+} else {
+    $description = "";
+}
+
+if(isset($_POST['Submit1'])){ 
+    $filepath = "uploads/" . $_FILES["file"]["name"];
+    
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) {
+        echo "<img src=".$filepath." height=300 width=300 />";
+    } else {
+        echo "Error !!";
+    }
+} 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -53,13 +27,18 @@ if ($uploadOk == 0) {
     <title>Image upload</title>
 </head>
 <body>
-    <div class = "upload__photo">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="fileToUpload" id="fileToUpload">
-             <input type="submit" value="Upload Image" name="submit">
-        </form>
-
-    </div>
     
+    <div>
+         <?php echo $description;?>
+    </div>
+
+    <form action="upload.php" enctype="multipart/form-data" method="post">
+        <input type="file" name="file"><br/>
+        <textarea name="description" rows="6" cols="40"></textarea><br/>
+        <input type="submit" value="Upload" name="Submit1"> <br/>
+        
+    </form>
+
+ 
 </body>
 </html>
