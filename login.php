e66a893d0e7e4fcc7e35f7ca75aef44e4883be6b
lissethys <r0448961@student.thomasmore.be>
2019-04-17 20:10:29 +0200

    layout aangepast

--- login.php
+++ login.php
@@ -38,7 +38,7 @@
 <head>
   <meta charset="UTF-8">
   <title>IMDFlix</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<div class="netflixLogin">
@@ -49,27 +49,29 @@
 				<?php if (isset($error)): ?>
 				<div class="form__error">
 					<p>
-						Sorry, we can't log you in with that email address and password. Can you try again?
+						Sorry, we can't log you in with that email address <br> and password. Can you try again?
 					</p>
 				</div>
 				<?php endif; ?>
 
 				<div class="form__field">
-					<label for="Email">Email</label>
-					<input type="text" name="email">
+					<label for="Email" class="label">Email</label>
+					<input type="text" name="email" class="input">
 				</div>
 				<div class="form__field">
-					<label for="Password">Password</label>
-					<input type="password" name="password">
+					<label for="Password" class="label">Password</label>
+					<input type="password" name="password" class="input">
 				</div>
 
 				<div class="form__field">
-					<input type="submit" value="Sign in" class="btn btn--primary">	
-					<input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label>
+					<input type="submit" value="Sign in" class="btn btn--primary">
+					<div class="center-div">	
+					<input type="checkbox" id="rememberMe"><label for="rememberMe" >Remember me</label>
+					</div>
 				</div>
 
 				<div>
-					<p>No account yet?<a href="register.php">Sign up here</a></p>
+					<p>No account yet? <a href="register.php">Sign up here</a></p>
 				</div>
 			</form>
 		</div>
--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@ if(isset($_POST['Submit1']) && !empty($_POST['description'])){
     $filepath = "uploads/" . $_FILES["file"]["name"];
     $description = $_POST['description'];
     if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath) && !empty($description)) {
-        echo "<img src=".$filepath." height=300 width=300 />";
+        $image = "<img src=".$filepath." height=200 width=200 />";
     } 
 }  else {
     $description = "Please add a description";
@@ -20,16 +20,27 @@ if(isset($_POST['Submit1']) && !empty($_POST['description'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Image upload</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 
+    <h2>Upload <br>an image</h2>
+
+    <div class="center-div-upload">
+    <?php echo $image ?>
+    </div>
+
     <form action="upload.php" enctype="multipart/form-data" method="post">
-        <input type="file" name="file"><br/>
+       
         <div>
-            <?php echo $description;?>
+            <p><?php echo $description;?></p>
         </div>
         <textarea name="description" rows="6" cols="40"></textarea><br/>
-        <input type="submit" value="Upload" name="Submit1"> <br/>
+        <div class="center-div-upload">
+        <input type="file" name="file" id="file" class="inputfile" />
+        <label for="file" class="inputfile-label">Choose a file</label>
+        </div>
+        <input type="submit" value="Upload" name="Submit1" class="btn btn--primary"> <br/>
         
     </form>
 
