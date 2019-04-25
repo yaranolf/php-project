2b72e830f1b33a3c4ca281919c1fcc8755671f84
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-04-25 19:55:45 +0200

    query toegevoegd

---
+++ functions.inc.php
@@ -0,0 +1,24 @@
+<?php
+    /*
+        this function checks if a user can login
+        and return TRUE or FALSE 
+    */
+    function canILogin( $username, $password ){
+        //$conn = Db::getInstance();
+        $conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter;", "root", "root", null);
+        
+        $query = "select * from users where email = '".$conn->real_escape_string($username)."'";
+        $result = $conn->query($query);
+        if( $result->num_rows == 1 ) {
+            $user = $result->fetch_assoc();
+            if( password_verify($password, $user['password']) ){
+                return true;
+            }   
+        }
+
+        return false;
+    
+    }
+
+
+?>
