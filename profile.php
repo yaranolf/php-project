ac3a1b61533bded385a3159b265615b296fcb8a0
Mathias <geertsmathias@gmail.com>
2019-05-02 11:44:12 +0200

    update profile

--- profile.php
+++ profile.php
@@ -1,9 +1,11 @@
 <?php
-include_once 'bootstrap.php';
-include_once 'classes/user.php';
 
+session_start();
+include_once 'classes/User.php';
+include_once 'bootstrap.php';
 $user = new User();
-$user->setUser_id(13);
+$user->setUser_id($_SESSION['user_id']);
+
 $profile = $user->getFirstName();
 
 if (!empty($_POST['edit'])) {
@@ -21,32 +23,38 @@ if (!empty($_POST['edit'])) {
     }
 
     $user_edit = new User();
-    $user_edit->setUser_id(13);
-    $user_edit->setFullname($_POST['fullname']);
-    $user_edit->setEmail($_POST['email']);
+    $user_edit->setUser_id($_SESSION['user_id']);
+    $user_edit->setFirstname($_POST['firstname']);
+    $user_edit->setLastname($_POST['lastname']);
+    if ($profile['email'] == $_POST['email']) {
+        $user_edit->setEmail($_POST['email']);
+    } elseif ($user_edit->emailExists($_POST['email'])) {
+        $user_edit->setEmail($profile['email']);
+    } else {
+        $user_edit->setEmail($_POST['email']);
+    }
     $user_edit->setBio($_POST['bio']);
     $user_edit->setImage($destination);
     if ($user_edit->update()) {
-        $message = 'Profile is up to date.';
+        $message = 'Your profile is updated.';
     } else {
-        $error = "Your update didn't come through.";
+        $error = "Something went wrong, profile isn't updated.";
     }
 }
 
-//password update
 if (!empty($_POST['passwordedit']) && !empty($_POST['password']) && !empty($_POST['repassword'])) {
     if (strcmp($_POST['password'], $_POST['repassword']) == 0) {
         $user_pass = new User();
-        $user_pass->setUser_id(13);
+        $user_pass->setUser_id($_SESSION['user_id']);
         $user_pass->setPassword($_POST['password']);
         if ($user_pass->updatePassword()) {
             $message = 'Password updated';
         }
     } else {
-        $error = 'Passwoorden moeten gelijk zijn';
+        $error = 'password must be the same';
     }
 } else {
-    $error = 'Invullen aub.';
+    $error = 'invullen';
 }
 
 $profile = $user->getFirstName();
@@ -62,43 +70,42 @@ $profile = $user->getFirstName();
 </head>
 <body>
 
+
+<?php include_once 'nav.inc.php'; ?>
+
+
+
+
     <form method="post" action="" enctype="multipart/form-data" class="edit_profile">
     <h2>Edit profile</h2>
     <label for="profileImg">Mijn profielfoto</label>
-    <img src="<?php echo $profile['image']; ?>" alt="">
+    <img src="<?php echo $profile['image']; ?>" alt="profiel" style="width:100px;">
     <input type="file" name="profileImg" id="profileImg" accept="image/gif, image/jpeg, image/png, image/jpg">
 
-    <div class="formitem">
-    <label for="fullname">fullname</label>
-    <input type="text" name="fullname" id="fullname" value="<?php echo $profile['fullname']; ?>">
-</div>
+    <label for="firstname">firstname</label>
+    <input type="text" name="firstname" id="firstname" value="<?php echo $profile['firstname']; ?>">
 
-<div class="formitem">
+    <label for="lastname">lastname</label>
+    <input type="text" name="lastname" id="lastname" value="<?php echo $profile['lastname']; ?>"> 
+
     <label for="bio">Bio</label>
     <textarea rows="4" cols="50" name="bio" id="bio"><?php echo $profile['bio']; ?></textarea>
-</div>
 
-<div class="formitem">
     <label for="email">E-mail</label>
     <input type="email" name="email" id="email" value="<?php echo $profile['email']; ?>">
-</div>
 
-    <input type="submit" name="edit" value="Update profile">
+    <input type="submit" name="edit" value="Edit">
 </form>
 
 <form method="post" action="" class="edit_profile">
     <h2>Wachtwoord aanpassen</h2>
-    <div class="formitem">
     <label for="password">New password</label>
     <input type="password" name="password" id="password" placeholder="New password">
-</div>
 
-<div class="formitem">
      <label for="repassword">Retype New password</label>
     <input type="password" name="repassword" id="repassword" placeholder="Retype New password">
-</div>
 
-    <input type="submit" name="passwordedit" value="Update password">
+    <input type="submit" name="passwordedit" value="Edit">
 </form>
 </body>
 </html>
