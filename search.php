bad5021514da0a55ee190ba02666f81029921386
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-03 14:26:35 +0200

    uit frontend gehaald + verplaatst naar klasse

--- search.php
+++ search.php
@@ -1,17 +1,6 @@
 <?php
 include 'classes/Search.php';
-// Users search terms is saved
-$searchIt = $_GET['searchIt'];
-// Create array for the names that are close to or match the search term
-$results = array();
-// Prepare statement
-$search = $db->prepare("SELECT * FROM 'users' WHERE 'img_description' LIKE ?");
-// Execute with wildcards
-$search->execute(array("%$searchIt%"));
-// Echo results
-foreach ($search as $s) {
-    echo $s['img_description'];
-}
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
