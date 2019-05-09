54500c299096615c12f251add9985ef90b9769f8
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-09 12:02:00 +0200

    foreach detailpost

--- detailPost.php
+++ detailPost.php
@@ -2,6 +2,8 @@
 include_once 'bootstrap.php';
 include_once 'classes/DetailPost.php';
 
+$detailPost = new Detail();
+$detailPost->addDetailPost();
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,9 +18,22 @@ include_once 'classes/DetailPost.php';
 <?php include_once 'nav.inc.php'; ?>
 <!-- foto linken aan search oproep-->
 
+<?php foreach ($detailPost as $d):
+  $post = new Post();
+  $post->setUser_id($f['user_id']);
+  $post->setFile_path($f['file_path']);
+  $post->setImg_description($f['img_description']);
+
+  $t = $post->getDate_created();
+  $time_ago = strtotime($t);
+?>
 <article class="center-div-image">
-      <?php $detailPost = Detail::addDetailPost(); ?>
+<a href="detailPost.php?id=<?php echo $f['id']; ?>"><img src=" <?php echo 'uploads/'.$post->file_path; ?> "  height=300 width=300 alt=""> </a>
+      <p><?php echo $post->img_description; ?></p>
+      <p><?php echo $convertedDate = Post::convertTime($time_ago); ?></p>
+      <div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
 </article>
+<?php endforeach; ?>
 
 
   <script>
