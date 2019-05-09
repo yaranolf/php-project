ce29d881751b810be3c0d79e74bfdb45790d18fb
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-09 10:23:59 +0200

    in comment zetten

--- detailPost.php
+++ detailPost.php
@@ -1,7 +1,7 @@
 <?php
 if (!empty($_POST)) {
     $postId = $_POST['postId'];
-    $userId = 1;
+    $userId = $_POST['userId'];
 
     include_once '../bootstrap.php';
     $detailpost = new Detail();
@@ -9,29 +9,33 @@ if (!empty($_POST)) {
     $detailpost->setUserId($userId);
     $detailpost->addDetail();
 }
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Detail post</title>
 </head>
 <body>
+
+<?php include_once 'nav.inc.php'; ?>
 <!-- foto linken aan search oproep-->
 
 
 <!-- commentaar eronder -->
-<?php foreach ($comment as $c):
+<?php //foreach ($comment as $c):
       //new comment
-      $comment = new Comment();
+      //$comment = new Comment();
       //id
-      $comment->setUserId($c['userId']);
-      $comment->setPostId($c['postId']);
-      $comment->setComment();
+      //$comment->setUserId($c['userId']);
+      //$comment->setPostId($c['postId']);
+      //$comment->setComment();
 
       //user ophalen
-      $user = new User();
+      //$user = new User();
       //new user
       //set id
       //set data
@@ -45,7 +49,7 @@ if (!empty($_POST)) {
         <input type="submit" value="add" id="submit_comment">
       </form>
     </div>
-  <?php endforeach; ?> 
+  <?php; // endforeach;?> 
 
   <script>
     //comment plaatsen >> kan ook verplaatsen naar detailpagina? 
