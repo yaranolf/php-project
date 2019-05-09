072f5eaf5bc79c95ab2ac237eb6e1a8e9d0804f5
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-09 10:12:14 +0200

    Create DetailPost.php
    detailpost

---
+++ classes/DetailPost.php
@@ -0,0 +1,60 @@
+<?php
+
+include 'classes/Post.php';
+
+class Detail
+{
+    private $postId;
+    private $userId;
+
+    /**
+     * Get the value of postId.
+     */
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+
+    /**
+     * Set the value of postId.
+     *
+     * @return self
+     */
+    public function setPostId($postId)
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userId.
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set the value of userId.
+     *
+     * @return self
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function addDetail()
+    {
+        $conn = Db::getInstance();
+        $statement = $conn->prepare('select * from posts where post_id = :postid');
+        $statement->bindValue(':postid', $this->id);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $result['addDetail'];
+    }
+}
