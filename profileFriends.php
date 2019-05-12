42e711efd6235eb9b8802a3757e4f028505e0c93
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-12 21:23:21 +0200

    profileFriends.php
    profiel voor vrienden aangemaakt

--- profileFriends.php
+++ profileFriends.php
@@ -25,31 +25,25 @@ $posts = Post::getAllFromFriends($friendList, 0, 2);
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 
-  <title>Document</title>
+  <title><?php echo $post->user_name; ?></title>
 </head>
 <body>
 
   
   <?php include_once 'nav.inc.php'; ?>
 
-  <h2>Your <br> inspiration</h2>
+  <h2 class="username"><?php echo $post->user_name; ?></h2>
 
   
 <div id="resultlist">
   
-  <?php
-  foreach ($posts as $post):
-  $t = $post->getDate_created();
-  $time_ago = strtotime($t);
-  ?>
-    <article class="center-div-image">
-      <a href="profileFriends.php?id=<?php $post['id']; ?>"> <h3 class="username"><?php echo $post->user_name; ?></h3></a>
+
       <img src=" <?php echo 'uploads/'.$post->file_path; ?> "  height=300 width=300 alt=""> 
       <p><?php echo $post->img_description; ?></p>
       <p><?php echo $convertedDate = Post::convertTime($time_ago); ?></p>
       <div><a href="#" class="like" data-id="<?php echo $post->id; ?>" >Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
     </article>
-  <?php endforeach; ?>
+
   </div>
     <input type="hidden" id="start" name="start" value="2"/>
     <input type="hidden" id="ids" value="<?php echo $friendList; ?>">
