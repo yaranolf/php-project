4ca027bbe3b25089d273ccf4fb8d6fe54f4d10ba
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-15 21:47:24 +0200

    link ook in index naar detailpagina

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $posts = Post::getAllFromFriends($friendList, 0, 2);
   ?>
     <article class="center-div-image">
       <a href="profileFriends.php?id=<?php echo $post->user_id; ?>"> <h3 class="username"><?php echo $post->user_name; ?></h3></a>
-      <img src=" <?php echo 'uploads/'.$post->file_path; ?> "  height=300 width=300 alt=""> 
+      <a href="detailPost.php?id=<?php echo $post->getId(); ?>"><img src=" <?php echo 'uploads/'.$post->file_path; ?> "  height=300 width=300 alt=""> </a>
       <div><a href="#" data-id="<?php echo $user->id; ?>" class="username"><?php echo $post->user_id; ?></a></div>
       <p><?php echo $post->img_description; ?></p>
       <p>(<?php echo $post->location; ?>)</p>
