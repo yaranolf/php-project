19a8e5a9a82fbed1aa9dffad95f938d451ad13f1
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-15 17:39:52 +0200

    database

--- inspiration_hunter_2019-04-25.sql
+++ inspiration_hunter_2019-04-25.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 06, 2019 at 10:23 AM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Generation Time: May 15, 2019 at 03:39 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -13,6 +13,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `inspiration_hunter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -33,10 +46,10 @@ CREATE TABLE `friends` (
 --
 
 INSERT INTO `friends` (`id`, `user_one`, `user_two`, `official`, `date_made`) VALUES
-(26, 2, 16, 1, '2019-05-05'),
-(27, 4, 16, 1, '2019-05-05'),
-(40, 17, 16, 1, '2019-05-05'),
-(41, 17, 18, 1, '2019-05-06');
+(1, 2, 16, 1, '2019-05-05'),
+(2, 4, 16, 1, '2019-05-05'),
+(6, 17, 18, 1, '2019-05-09'),
+(7, 19, 17, 1, '2019-05-12');
 
 -- --------------------------------------------------------
 
@@ -47,6 +60,7 @@ INSERT INTO `friends` (`id`, `user_one`, `user_two`, `official`, `date_made`) VA
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
   `img_description` varchar(255) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `file_path` varchar(255) NOT NULL
@@ -56,13 +70,13 @@ CREATE TABLE `images` (
 -- Dumping data for table `images`
 --
 
-INSERT INTO `images` (`id`, `user_id`, `img_description`, `date_created`, `file_path`) VALUES
-(5, 1, 'vacay', '2019-04-27 15:12:48', '_DSC4743.jpeg'),
-(9, 2, 'fruit', '2019-04-27 18:25:42', '09f7e88ccfe2e64f4baa408399765d9b.jpg'),
-(10, 16, 'what a view!', '2019-04-27 19:09:00', '_04A7147.jpg'),
-(11, 4, 'test', '2019-05-03 13:22:39', 'large (10).jpg'),
-(12, 0, 'blabla', '2019-05-03 13:22:48', '_DSC4743.jpeg'),
-(13, 17, 'testtest', '2019-05-03 13:41:54', 'DSC01936.jpg');
+INSERT INTO `images` (`id`, `user_id`, `user_name`, `img_description`, `date_created`, `file_path`) VALUES
+(5, 17, 'Noorve', 'vacay', '2019-04-27 15:12:48', '11.jpeg'),
+(9, 17, 'Noorve', 'Fruit', '2019-04-27 18:25:42', '32.jpg'),
+(10, 16, 'Banciu', 'what a view!', '2019-04-27 19:09:00', '_04A7147.jpg'),
+(20, 17, 'Banciu', 'Beach', '2019-05-09 06:18:13', 'photo-1469854523086-cc02fe5d8800-min.jpeg'),
+(24, 17, 'Noorve', 'Mountains', '2019-05-09 06:54:03', 'photo-1467396555244-ddb071a5841d.jpeg'),
+(25, 18, 'Banciu', 'Airplane', '2019-05-09 07:02:17', 'photo-1517479149777-5f3b1511d5ad.jpeg');
 
 -- --------------------------------------------------------
 
@@ -83,7 +97,11 @@ CREATE TABLE `likes` (
 
 INSERT INTO `likes` (`id`, `post_id`, `user_id`, `date_created`) VALUES
 (4, 5, 1, '2019-05-03 15:43:58'),
-(5, 5, 1, '2019-05-03 15:44:02');
+(5, 5, 1, '2019-05-03 15:44:02'),
+(6, 5, 1, '2019-05-05 16:13:14'),
+(7, 5, 1, '2019-05-05 16:13:16'),
+(10, 0, 17, '2019-05-15 13:51:46'),
+(11, 5, 17, '2019-05-15 17:36:25');
 
 -- --------------------------------------------------------
 
@@ -105,17 +123,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`) VALUES
-(2, 'Yara', 'Nolf', 'yaranolf', 'yaranolf@telenet.be', '$2y$14$MwUThUr5Rk40WRFTTKbHZu3DEHQ9ZfJggDM3kals3p7I8EZp1.A1m'),
-(4, 'Liese', 'Vanhecke', 'liesevanhecke', 'liesevanhecke@hotmail.com', '$2y$14$pewnkOZ6hkTk45qQi.VIduP3QtCJ6XeSTSlWq.41sbvUlsHTZoSlW'),
-(8, 'Annelies', 'Driessen', 'Annelies', 'Driessen@gmail.com', '$2y$14$aHgtdWDRLQ2v/Xq7hICJsem1K6VzFOzQZEDpOdAYMwleGLVkD.bUi'),
-(15, 'Lisse', 'Thys', 'Lisse', 'lisse.thys@hotmail.com', '$2y$10$zwR0hPfZxBTH8Xl.2GStVOfOvg90hhzTE4RlwrYZCFkDRhxCI6Tvu'),
-(16, 'Mathias', 'Geerts', 'Mathias', 'geertsmathias@gmail.com', '$2y$10$c0SG8zSq6vJdmsDagumVh.9BBAIFI8Fp4uxufksAOvk73v1nvTru2'),
-(17, 'Raf', 'Snijders', 'Rafke', 'raf@gmail.com', '$2y$10$01CPBngCaycBsgX8QZ1qI..cbJzNPRFbWgoI3xcIKzEzxfUydn9zC'),
-(18, 'Emiel', 'Geerts', 'Emiel', 'emiel@gmail.com', '$2y$10$k3J9N0z0b5UF2dFDywbeE.2DZlyuRJpJbz7bRn9oeKhG7ts4ug6rC');
+(16, 'test', 'test', 'test', 'test@test.com', '$2y$10$8gSNoiBii3xCUnD6u8Z2IOTAlCDscjXcjwEyTq0yvptZGXlLMqDwa'),
+(17, 'Noortje', 'Veenhuizen', 'Noorve', 'noortjeveenhuizen@hotmail.com', '$2y$10$VEzHdhroQjTQ7HnC4OKiJuNPDDstvzkZVxEQCXl1sybhG2E57fgB2'),
+(18, 'Sarah', 'Van den Heuvel', 'Sarahvh', 'sarahvandenheuvel@hotmail.com', '$2y$10$bcEt632FKpDDdElcSI4jKO/V/BRozHIsLMfPi2ZRehcE9Il7mmwpa'),
+(19, 'Andreas', 'Banciu', 'Banciu', 'andreasbanciu@live.be', '$2y$10$IPqhyftGuKJ/g3ovR8u13un1iLfkv6EjNjs89cKiRBm4MGu3Q66lG');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `friends`
@@ -146,25 +167,31 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
