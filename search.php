046a08972d9c722f2ab7965b1a3472864ccfd6c4
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-19 20:39:36 +0200

    script in search.php

--- search.php
+++ search.php
@@ -28,6 +28,7 @@ $user = User::getUser();
     $post = new Post();
     $post->setId($f['id']);
     $post->setUser_id($f['user_id']);
+    $post->setUser_name($f['user_name']);
     $post->setFile_path($f['file_path']);
     $post->setImg_description($f['img_description']);
 
@@ -35,39 +36,29 @@ $user = User::getUser();
     $time_ago = strtotime($t);
   ?>
     <article class="center-div-image">
+      <a href="profileFriends.php?id=<?php echo $post->user_id; ?>"> <h3 class="username"><?php echo $post->user_name; ?></h3></a>
       <a href="detailPost.php?id=<?php echo $post->getId(); ?>"><img src=" <?php echo 'uploads/'.$post->file_path; ?>" height=300 width=300 alt=""> </a>
       <p><?php echo $post->img_description; ?></p>
       <p><?php echo $convertedDate = Post::convertTime($time_ago); ?></p>
-      <div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
+      <div class="center-div"><a href="#" class="like btn--secondary" data-id="<?php echo $post->id; ?>" >Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
+      <div class="center-div"><a href="#" class="report btn--secondary" data-id="<?php echo $post->id; ?>" >Inappropriate</a> <span class='inappropriate'><?php echo implode($post->getNrOfInappropriate()); ?></span> people report this </div>
     </article>
 <?php endforeach; ?>
 
-  <script>
-    $(document).ready(function(){
-            $(".like").on("click", function(e){
-                var button = $(this);
-                var postId = $(this).data('id');
-                var elLikes = $(this).parent().find(".likes");
-                var likes = elLikes.html();
-    
-                $.ajax({
-                    method: "POST",
-                    url: "ajax/like.php",
-                    data: { postId: postId },
-                    dataType: "json",
-                    
-                })
-                .done(function( res ) {
-                    if(res.status === "liked") {
-                      //button.html("unlike");
-                      elLikes.html(likes);
-                    }
-                });
-    
-                e.preventDefault();
-            });
-          });
-  </script>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+
+  <script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
+<script src="js/Posts.js" ></script>
+<script src="js/Reports.js" ></script>
+<script src="js/Likes.js" ></script>
 
 </body>
 </html>
