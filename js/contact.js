6fbb13a14ee897fb33c3fb80b1cad10f722832c9
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-20 16:34:54 +0200

    submit knop

--- js/contact.js
+++ js/contact.js
@@ -6,5 +6,13 @@ $(function() {
     // Get the messages div.
     var formMessages = $('#form-messages');
 
-    // TODO: The rest of the code will go here...
+// Set up an event listener for the contact form.
+$(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+
+
+
+});
+    
 });
