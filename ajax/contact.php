cf4e921b79a925daee0edb5d17ecefa84bfd35dd
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-20 17:10:04 +0200

    spaties toegevoegd

--- ajax/contact.php
+++ ajax/contact.php
@@ -6,28 +6,20 @@
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        if (empty($name) or empty($message) or empty($email)) {
-            $error = 'Please try again';
-            exit;
-        }
-
         // Set the recipient email address.
         $recipient = 'r0583287@student.thomasmore.be';
 
         $subject = "New contact from $name";
 
         $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
+        $email_content .= "Email: $email\n";
         $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
         $email_headers = "From: $name <$email>";
 
-        // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
-        } else {
-            $error = 'something went wrong, try again!';
+            //send
         }
     } else {
-        $error = 'Please try again';
+        echo $error = 'Please try again';
     }
