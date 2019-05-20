bb70fd13baf82dffbf998cb5517e7685d28d2808
noortjeveenhuizen <noortjeveenhuizen@hotmail.com>
2019-05-20 16:35:47 +0200

    .fail met succes en error

--- js/contact.js
+++ js/contact.js
@@ -18,7 +18,7 @@ $(form).submit(function(event) {
         url: $(form).attr('action'),
         data: formData
     })
-    
+
     .done(function(response) {
         // Make sure that the formMessages div has the 'success' class.
         $(formMessages).removeClass('error');
@@ -32,7 +32,19 @@ $(form).submit(function(event) {
         $('#email').val('');
         $('#message').val('');
     })
-   
+    
+   .fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+    } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+});
 
 });
     
